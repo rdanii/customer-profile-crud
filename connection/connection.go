1c519760b46265eedb5a751078537a75e34df7f9
rdanii <dani.rahmat7398@gmail.com>
2022-03-26 15:06:26 +0700

    update db name

--- connection/connection.go
+++ connection/connection.go
@@ -14,7 +14,7 @@ var (
 )
 
 func Connect() {
-	DB, Err = gorm.Open("mysql", "root:@/dani?charset=utf8&parseTime=True"+"&loc=Local"+"&timeout=10s")
+	DB, Err = gorm.Open("mysql", "root:@/dani?charset=utf8&parseTime=True")
 
 	if Err != nil {
 		log.Println("Connection failed", Err)
